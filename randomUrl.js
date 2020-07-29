966cf53e22e21a6df63c12b422f5d0c1df8be1dc
Maxim <chexovm@list.ru>
2020-07-29 04:09:38 +0300

    added comments just in case

--- randomUrl.js
+++ randomUrl.js
@@ -1,11 +1,14 @@
+//importing filesystem and working with the 'word' txt file
 import * as fs from "fs";
 const wordsArray = fs.readFileSync("words.txt", "utf8").split("\r\n");
 
+//this is just a function that generates a random number in between min/max params
 function randomNumber(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
+//function itself
 export default function generateUrl() {
   let url = "";
   for (let i = 0; i < 6; i++) {
